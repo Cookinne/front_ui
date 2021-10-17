5ea45c345b054e6aecd6bf98899db45934350e2e
jinghaihong <jinghh8@163.com>
2021-10-17 18:09:46 +0800

    Steps

--- src/containers/Page2/index.js
+++ src/containers/Page2/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Skeleton } from 'antd';
+import { Steps } from 'antd';
 import style from './style.scss';
+
+const { Step } = Steps;
 
 const Page2 = (props) => {
   useEffect(() => {
@@ -10,9 +12,11 @@ const Page2 = (props) => {
   return (
     <div className={style.container}>
       <h2>Page2</h2>
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
+      <Steps style={{ width: '70%', margin: 'auto', marginTop: 40 }} current={1}>
+        <Step title="完成" description="任务已完成" />
+        <Step title="执行中" subTitle="Left 00:00:08" description="任务执行中" />
+        <Step title="等待" description="任务等待中" />
+      </Steps>
     </div>
   );
 };
